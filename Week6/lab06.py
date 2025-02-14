b70ec373db437dff6b1f165402096979b5beb923
DistinctDynamo <nguyensteven578@gmail.com>
2025-02-14 08:51:47 -0500

    Lab 6 question 5a

--- Week6/lab06.py
+++ Week6/lab06.py
@@ -253,4 +253,5 @@ if not input_invalid:
     if not input_invalid:
         stars_display = "*" * num_stars
         print("    |    Hero " + short_name + " gets <" + stars_display + "> stars")
-        # Lab 06 - Question 3 and 4    
+        # Lab 06 - Question 3 and 4
+        functions_lab06.save_game(winner, hero_name=short_name, num_stars=num_stars)    
