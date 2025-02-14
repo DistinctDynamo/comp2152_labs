b0e19416eb5a5374a23e019e4b6f2456f7df212c
DistinctDynamo <nguyensteven578@gmail.com>
2025-02-14 09:27:01 -0500

    Completed lab 6 question 6

--- Week6/lab06.py
+++ Week6/lab06.py
@@ -180,8 +180,26 @@ if not input_invalid:
 
 
     # Call Recursive function
-    print("    |", end="    ")
-    num_dream_lvls = input("How many dream levels do you want to go down?")
+    num_dream_lvls=-1
+    while(num_dream_lvls<0 or num_dream_lvls>3):     
+        print("    |", end="    ")
+        num_dream_lvls = input("How many dream levels do you want to go down?")
+        if(num_dream_lvls ==0):
+            num_dream_lvls = -1
+            print("Number should be a whole number between 0-3, try again")
+        else:
+            num_dream_lvls=int(num_dream_lvls)
+            if(num_dream_lvls<0 or num_dream_lvls>3):
+                num_dream_lvls = -1
+                print("Number should be a whole number between 0-3, try again")
+            elif(not num_dream_lvls ==0):
+                health_points -=1
+                crazy_level = functions_lab06.inception_dream(num_dream_lvls)
+                combat_strength += crazy_level
+                print("Combat strength: " + str(combat_strength))
+                print("Health point: " + str(health_points))
+        print("num dream lvs:" + num_dream_lvls)
+
     if num_dream_lvls != 0:
         health_points -= 1
         crazy_level = functions_lab06.inception_dream(num_dream_lvls)
