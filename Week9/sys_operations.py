c16c9106ddaf0b67ffc5387d42b16b2c1d43d29c
DistinctDynamo <nguyensteven578@gmail.com>
2025-03-07 08:41:55 -0500

    continuing lab week 9

---
+++ Week9/sys_operations.py
@@ -0,0 +1,13 @@
+import platform
+import socket
+
+print("Current Machine Type")
+print(platform.machine())
+print("===============================")
+
+print("Current Processor Type")
+print(platform.architecture())
+print("===============================")
+
+print("")
+print(socket.setdefaulttimeout(50))
