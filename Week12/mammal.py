43d156c0178e876a156f7dd2bdfebf9115bd9b75
DistinctDynamo <nguyensteven578@gmail.com>
2025-03-28 08:29:34 -0400

    updated mammal

--- Week12/mammal.py
+++ Week12/mammal.py
@@ -1,13 +1,25 @@
 from heart import Heart
 
 class Mammal:
-    def __init__():  # Aggregation through parameter
+    def __init__(self,p_age,tick=None):  # Aggregation through parameter
         print("Constructor: Inside the Parent class constructor: Making the Mammal part of the object")
+        self.page = p_age
+        self.__live_birth = True
+
+        self.heart = Heart()
+
+        self.tick = tick
 
     def __del__(self):
         print("Destructor: The garbage collector is now deleting the Mammal part of the object")
 
-
+    @property
+    def live_birth(self):
+        return self.__live_birth
+    
+    @live_birth.setter
+    def live_birth(self,p_live_birth):
+        self.__live_birth=p_live_birth
 
     def __str__(self):
         tick_status = "attached" if self.tick else "none"
